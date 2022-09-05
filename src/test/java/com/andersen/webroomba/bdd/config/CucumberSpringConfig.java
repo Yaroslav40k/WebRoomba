9ec3aa15fb8cc02b6372a9bff9668e0c9f80fc2e
Yaroslav Dmitriev <Yaroslav.Dmitriev@yoti.com>
2022-09-05 13:02:46 +0300

    Add Cucumber framework support
    - added Spring container support instead of Pico container, because we already have Spring in our project

---
+++ src/test/java/com/andersen/webroomba/bdd/config/CucumberSpringConfig.java
@@ -0,0 +1,9 @@
+package com.andersen.webroomba.bdd.config;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class CucumberSpringConfig {
+}
