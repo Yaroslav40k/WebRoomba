bc7ca3f28e4420b2b92dc34824c62733774b7721
Yaroslav Dmitriev <Yaroslav.Dmitriev@yoti.com>
2022-09-05 13:26:58 +0300

    Fix old broken unit tests
    - Old tests don't know anything about a dirt container, so we have to create some mocks in order to make them work properly

--- src/test/java/com/andersen/webroomba/HooverWorkResultAcquirerServiceImplTest.java
+++ src/test/java/com/andersen/webroomba/HooverWorkResultAcquirerServiceImplTest.java
@@ -4,6 +4,8 @@ import com.andersen.webroomba.model.Hoover;
 import com.andersen.webroomba.model.implementation.GridCell;
 import com.andersen.webroomba.model.implementation.GridCellAddress;
 import com.andersen.webroomba.model.implementation.RoboticHoover;
+import com.andersen.webroomba.service.DirtContainerService;
+import com.andersen.webroomba.service.implemantation.HooverDirtContainerService;
 import com.andersen.webroomba.service.implemantation.HooverWorkResultAcquirerServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,11 +18,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 class HooverWorkResultAcquirerServiceImplTest {
 
     private final HooverWorkResultAcquirerServiceImpl hooverWorkResultAcquirerServiceImpl = new HooverWorkResultAcquirerServiceImpl();
+    private final DirtContainerService dirtContainerService = new HooverDirtContainerService();
 
     @ParameterizedTest
     @CsvSource({"1, 1", "5, 5","5, 6","5, 10", "100, 100", "1000, 10000"})
     void acquireHooverWorkResults_fromLegalFinishPosition_expectingPositiveResult(int xCoordinate, int yCoordinate) {
         Hoover hoover = new RoboticHoover();
+        hoover.setDirtContainer(dirtContainerService.createDirtContainer());
         hoover.setLocation(new GridCell(new GridCellAddress(xCoordinate,yCoordinate),false));
         Assertions.assertDoesNotThrow(() -> hooverWorkResultAcquirerServiceImpl.acquireExecutionResults(hoover));
     }
