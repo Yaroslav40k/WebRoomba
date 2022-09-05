068883c73e84a34037075429e3cc2e608d3a14df
Yaroslav Dmitriev <Yaroslav.Dmitriev@yoti.com>
2022-09-05 13:46:57 +0300

    Add Java Docs
    - This test project lacks Java docs. This fact notwithstanding, it would be a good practice to add some, as a final part of a task. Such docs will help other developers understand your code better.
    Just follow some simple rules, i.e. don't say 'how' (one can see that in the code), but 'what' and 'why.'
    
    - Forgive me for this little 'Batman' Easter egg. I know that it breaks the major 'Clean Code' rule, but this is my test project, so I think I can indulge in a small pleasure.

--- src/main/java/com/andersen/webroomba/service/implemantation/HooverDirtContainerService.java
+++ src/main/java/com/andersen/webroomba/service/implemantation/HooverDirtContainerService.java
@@ -6,14 +6,28 @@ import com.andersen.webroomba.model.implementation.HooverDirtContainer;
 import com.andersen.webroomba.service.DirtContainerService;
 import org.springframework.stereotype.Service;
 
+/**
+ * The{@code HooverDirtContainerService} represents a command panel for a dirt container instance.
+ */
 @Service
 public class HooverDirtContainerService implements DirtContainerService {
 
+    /**
+     * Creates an eternal dirt container of unlimited size.
+     *
+     * @return created dirt container
+     */
     @Override
     public DirtContainer createDirtContainer() {
         return new HooverDirtContainer();
     }
 
+    /**
+     * Removes all dirt from a container. Can't store dirt somewhere, so  while using
+     * this method think about it like "you are going to make dirt disappear!"(C).
+     *
+     * @param testHoover hoover, you are going to free from dirt
+     */
     @Override
     public void cleanContainer(Hoover testHoover) {
         DirtContainer dirtContainer = testHoover.getDirtContainer();
